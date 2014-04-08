ace869fd7619b42c2aa58d131dbd531d1fda18ba
Nhan Dao <nhan.t.dao@gmail.com>
2014-04-08 10:34:37 -0700

    Initial Commit

---
+++ commadline/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  commadline
+//
+//  Created by Admin on 4/8/14.
+//  Copyright (c) 2014 Nhan Dao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

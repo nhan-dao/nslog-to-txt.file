f371c2012cbb9bad0954ab68379d9b24b673c2ec
Nhan Dao <nhan.t.dao@gmail.com>
2014-04-08 11:04:23 -0700

    This is to log everything in the command line into a txt file.

---
+++ commadline/Log.h
@@ -0,0 +1,13 @@
+//
+//  Log.h
+//  commadline
+//
+//  Created by Admin on 4/8/14.
+//  Copyright (c) 2014 Nhan Dao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Log : NSObject
+
+@end
---
+++ commadline/Log.m
@@ -0,0 +1,13 @@
+//
+//  Log.m
+//  commadline
+//
+//  Created by Admin on 4/8/14.
+//  Copyright (c) 2014 Nhan Dao. All rights reserved.
+//
+
+#import "Log.h"
+
+@implementation Log
+
+@end
